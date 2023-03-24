3e5428dd6ebd71f2c01200ce3507dd343b43eb1d
LakshayBhandari <lakshaybhandari867@gmail.com>
2023-03-24 12:02:30 +0530

    initialcommit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'e3f779e678msh3eb055f54766317p16de9ajsn358084293189',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
